179f28961ecd49a4f395dc923922cf2bf887c76d
hiroki0816 <hiroki.oosugi@gmail.com>
2020-09-06 14:42:07 +0900

    create countup app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from "react-dom";
 import './App.css';
+import CounterApp from './counterApp';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Hello React</p>
-      <p>Hello React</p>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state={
+      todoList: [],
+      value: '',
+    }
+  }
+
+  onChange(e) {
+    this.setState({value : e.target.value})
+  }
+
+  add() {　
+    this.setState({
+      todoList: this.state.todoList.concat(this.state.value),
+      value: '',
+    })
+    
+  }
+
+  render() {
+    const todoListNode = this.state.todoList.map((todo, idx) => {
+      return <li key={idx}>{todo}</li>
+    })
+
+    return (
+      <div>
+        <h1>TODO App</h1>
+        <input 
+          type="text"
+          value={this.state.value}
+          onChange={e => this.onChange(e)}
+        />
+        <button onClick={() => this.add()}>追加</button>
+        <ul>
+          {todoListNode}
+        </ul>
+      </div>
+    )
+  }
 }
+
+
+
 
 export default App;
